607b8f8590a92ee971f715809a107f68cd915f04
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-24 19:51:41 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  array.collect{|n| n ** 2}
-#  squared_array = []
-#  array.each do |num|
-#    squared_array << num ** 2
-#  end
-#  squared_array
+#  array.collect{|n| n ** 2}
+  squared_array = []
+  array.each do |num|
+    squared_array << num ** 2
+  end
+  squared_array
 end
